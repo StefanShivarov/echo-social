e5ea869607c1088aac61872e4a8c7520c044d27a
StefanShivarov <stefan.shivarov.jr@gmail.com>
2024-12-18 17:51:02 +0200

    add user model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const { DataTypes, Model } = require("sequelize");
+const { sequelize } = require("../config/databaseConfig.js");
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "User",
+    tableName: "users",
+  }
+);
+
+module.exports = User;
---
+++ models/index.js
@@ -0,0 +1,4 @@
+const { sequelize } = require("../config/databaseConfig");
+const User = require("./User");
+
+module.exports = { User };
