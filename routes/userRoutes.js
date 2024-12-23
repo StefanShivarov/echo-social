ea8eab586ed43ea52639dc9f93013717266dfbcd
StefanShivarov <stefan.shivarov.jr@gmail.com>
2024-12-23 21:30:15 +0200

    delete old files

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const { showSignInForm, createUser } = require("../controllers/userController");
-
-router.get("/signin", showSignInForm);
-
-router.post("/signin", createUser);
-
-module.exports = router;
