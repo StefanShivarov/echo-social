5b012316faca5050aaaba901823eda724455650e
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2025-02-02 14:39:44 +0200

    add more methods for fetching following data from database

--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -1,24 +1,74 @@
+const { Op } = require("sequelize");
 const User = require("../models/User");
+const Follow = require("../models/Follow");
 
 const userRepository = {
-  findUserById: async (id) => {
+  findAllUsersExcept: (id) => {
+    return User.findAll({
+      where: { id: { [Op.ne]: id } },
+    });
+  },
+
+  findUserById: (id) => {
     return User.findByPk(id);
   },
 
-  findUserByEmail: async (email) => {
+  findUserByEmail: (email) => {
     return User.findOne({ where: { email } });
   },
 
-  createUser: async (user) => {
+  createUser: (user) => {
     return User.create(user);
   },
 
-  updateUserById: async (id, updateData) => {
+  updateUserById: (id, updateData) => {
     return User.update(updateData, { where: { id }, returning: true });
   },
 
-  deleteUserById: async (id) => {
+  deleteUserById: (id) => {
     return User.destroy({ where: { id } });
+  },
+
+  getFollowersCount: (id) => {
+    return Follow.count({ where: { followingId: id } });
+  },
+
+  getFollowingCount: (id) => {
+    return Follow.count({ where: { followerId: id } });
+  },
+
+  findUsersNotFollowedBy: async (id) => {
+    return Follow.findAll({
+      where: { followerId: id },
+      attributes: ["followingId"],
+    }).then((follows) => {
+      const followedUserIds = follows.map((follow) => follow.followingId);
+      return User.findAll({
+        where: {
+          id: { [Op.notIn]: followedUserIds.concat(id) },
+        },
+      }).catch((err) => {
+        throw err;
+      });
+    });
+  },
+
+  findUsersFollowedBy: async (id) => {
+    return User.findAll({
+      include: {
+        model: User,
+        as: "followers",
+        through: { attributes: [] },
+        where: { id: id },
+      },
+    });
+  },
+
+  findFollowersForUser: async (id) => {
+    return Follow.findAll({
+      where: { followerId: id },
+      include: [{ model: User, as: "follower" }],
+    }).then((follows) => follows.map((follows) => follows.follower));
   },
 };
 
