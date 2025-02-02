581ab161db84cf303894a078d9c1ec9549031b79
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2025-02-02 14:41:09 +0200

    add routes for followings and profile

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,7 +1,17 @@
 const router = require("express").Router();
-const { showSignUpForm, signUpUser } = require("../controllers/userController");
+const {
+  showSignUpForm,
+  signUpUser,
+  showUserProfile,
+  showFollowing,
+  showFollowers
+} = require("../controllers/userController");
 
 router.get("/signup", showSignUpForm);
 router.post("/signup", signUpUser);
+
+router.get("/profile", showUserProfile);
+router.get("/users/:id/followers", showFollowers);
+router.get("/users/:id/following", showFollowing);
 
 module.exports = router;
