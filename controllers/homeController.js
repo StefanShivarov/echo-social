377fdcfc644946d34ec20affad9a9561ec5c7a8e
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2025-02-08 19:58:27 +0200

    visualize posts of followed users on home page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,11 +1,14 @@
+const postService = require("../services/postService");
+const userService = require("../services/userService");
 const { findOtherUsers } = require("../services/userService");
 
 const showHomepage = async (req, res) => {
   if (!req.isAuthenticated()) {
     return res.render("landing");
   } else {
-   // const users = await findOtherUsers(req.user.id);
-    return res.render("index", { user: req.user });
+    const followedUsersIds = req.user.followedUsers.map((user) => user.id);
+    const followedUsersPosts = await postService.getPostsFromUsers(followedUsersIds);
+    return res.render("index", { user: req.user, followedUsersPosts });
   }
 };
 
