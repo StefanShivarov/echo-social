db09e069cf0367a44a7264b85bdc7c794b748587
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2025-02-09 02:40:16 +0200

    add post route for creating a comment

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -3,9 +3,11 @@ const {
   showPostDetails,
   createPost,
   showCreatePostForm,
+  createComment
 } = require("../controllers/postController");
 
 router.get("/posts/:id", showPostDetails);
 router.get("/create-post", showCreatePostForm);
 router.post("/posts", createPost);
+router.post("/posts/:id/comments", createComment);
 module.exports = router;
